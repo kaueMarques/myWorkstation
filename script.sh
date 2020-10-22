5d66eb2c872057019f2fb9a18cbd60d6009d4844
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2020-10-22 16:42:59 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,56 +1,91 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-sudo apt -y update && sudo apt -y upgrade &&
-
-DOWNLOAD_TMP="/tmp"
+#################### Variables ####################  
 DOWNLOADER_DISTRO_DEB="sudo apt install -y "
-DOWNLOADER_REPOSITORY_GITHUB="git clone "
 
-URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+#################### Arrays ####################  
 
 DISTRO_APPS=(
-    'curl'
-    'neovim'  
+    #Development
     'code'
+    'neovim'  
     'nodejs'
     'npm'
     'python3'
     'python3-pip'
-    'deepin-terminal'
-    'transmission'
     'gnome-boxes'
-    'gnome-clocks'
-    'gnome-tweak-tool'   
-    'vlc'
     'git'
     'atom'
-    'flatpak'
     'zsh'
+    'openjdk-11-jdk'
+    'docker.io'
+    'google-chrome-stable'
+    'mariadb-server'
+    'mariadb-client'
+
+    #Others
+    'firefox'
+    'curl'
+    'deepin-terminal'
+    'transmission'
+    'gnome-clocks'   
+    'vlc'   
+    'flatpak'
     'lutris'
     'steam-devices'
     'steam-installer'
     'steam:i386'
     'playonlinux'
-    'openjdk-11-jdk'
     'gnome-contacts'
+    'snapd'
+
+    #Customization
+    'gnome-tweak-tool'
+    'breeze-cursor-theme'
+    'yaru-theme'
+    'yaru-theme-gnome-shell'
+    'yaru-theme-gtk'
+    'yaru-theme-icon'
+    'yaru-theme-sound'
 )
 
-#FLATPAK
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
-flatpak install flathub org.apache.netbeans -y
+#################### Actions ####################  
+
+echo ' --- This 1st version was made for POP OS '
+
+## Removendo travas eventuais do apt ##
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
+
+echo ' --- Updating your OS'
+sudo apt -y update && sudo apt -y upgrade
+
+#Make my defaults paths
+mkdir ~/_ti ~/_ti/sudy ~/_ti/projects
+
+#Moving configs files
+mv .zshrc /$HOME
+
+#Snaps
+sudo snap install netbeans --classic
+sudo snap install spotify
+sudo snap install 
 
 
 for i in "${DISTRO_APPS[@]}";
 do  
     echo ""
-    echo " -- INTALING: $i "
+    echo " --- INTALING: $i "
     $DOWNLOADER_DISTRO_DEB $i
 done
 
+
 #Finishing
-echo " -- Terminando"
+echo ''
+echo " --- Finishing..."
 sudo apt update && sudo apt dist-upgrade -y
 flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
-echo " -- FINALIZADO"
+echo " --- Let's Rock! All done!"
+curl parrot.live
